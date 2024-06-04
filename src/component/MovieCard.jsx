cc82a8711eff0d1437e6d1127ee2be647efc01ef
juned1110 <junedking1110@gmail.com>
2024-06-04 15:31:14 +0530

    card UI changed

--- src/component/MovieCard.jsx
+++ src/component/MovieCard.jsx
@@ -16,17 +16,32 @@ const MovieCard = ({ movie, viewDetails }) => {
   };
 
   return (
-    <div className="border p-2 m-2 rounded-lg shadow-lg">
+    <div
+      className="p-2 m-2 rounded-lg shadow-xl mb-5 mt-10 bg-[#dddada] border-4 border-solid border-slate-200 hover:border-slate-300"
+      style={{
+        boxShadow:
+          "rgba(0, 0, 0, 0.17) 0px -2px 2px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -7px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 1px 2px, rgba(0, 0, 0, 0.09) 0px 2px 4px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 10px 10px",
+        boxShadow:
+          "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;",
+      }}
+    >
       <img
         src={movie.Poster}
         alt={movie.Title}
-        className="w-full h-64 object-cover"
+        className="w-full h-64 object-cover rounded-[30px]"
+        style={{
+          boxShadow: "rgb(38, 57, 77) 0px 20px 30px -10px",
+        }}
       />
-      <h2 className="text-xl mt-2">{movie.Title}</h2>
-      <p>{movie.Year}</p>
+      <h2 className="text-xl ml-3 mt-5 font-bold">{movie.Title}</h2>
+      <p className="font-bold ml-3">{movie.Year}</p>
       <button
         onClick={() => viewDetails(movie.imdbID)}
-        className="bg-blue-500 text-white p-2 rounded mt-2"
+        className="bg-blue-500 text-white p-2 rounded-md mt-2 ml-3 font-bold mb-3"
+        style={{
+          boxShadow:
+            "rgba(0, 0, 0, 0.17) 0px -2px 2px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -7px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 1px 2px, rgba(0, 0, 0, 0.09) 0px 2px 4px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 10px 10px",
+        }}
       >
         Details
       </button>
--- src/component/MovieDetails.jsx
+++ src/component/MovieDetails.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchMovieDetails } from "../store/moviesSlice";
+import img from "../assets/close.png";
 
 const MovieDetails = ({ selectedMovieId, closeDetails }) => {
   const dispatch = useDispatch();
@@ -13,35 +14,48 @@ const MovieDetails = ({ selectedMovieId, closeDetails }) => {
   }, [selectedMovieId, dispatch]);
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-75 flex items-center justify-center">
-      <div className="bg-white p-4 rounded-lg max-w-lg w-full">
-        <button onClick={closeDetails} className="text-right text-2xl">
-          &times;
+    <div className="fixed inset-0 bg-gray-950 bg-opacity-75 flex items-center justify-center">
+      <div className="bg-[#f1f1f1] p-4 rounded-lg max-w-lg w-full">
+        <button
+          onClick={closeDetails}
+          className="text-right rounded-2xl ml-[32vw]"
+          style={{
+            boxShadow:
+              "rgba(0, 0, 0, 0.17) 0px -2px 2px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -7px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 1px 2px, rgba(0, 0, 0, 0.09) 0px 2px 4px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 10px 10px",
+          }}
+        >
+          <img src={img} className="w-10" />
         </button>
         {details ? (
           <>
-            <h2 className="text-2xl">{details.Title}</h2>
+            <h2 className="text-2xl font-bold ml-[1vw] underline decoration-pink-500">
+              {details.Title}
+            </h2>
             <img
               src={details.Poster}
               alt={details.Title}
-              className="w-full h-64 object-cover mt-4"
+              className="w-full h-64 object-cover mt-4 rounded-lg mb-5"
+              style={{
+                boxShadow:
+                  "rgba(0, 0, 0, 0.17) 0px -2px 2px 0px inset, rgba(0, 0, 0, 0.15) 0px -36px 30px 0px inset, rgba(0, 0, 0, 0.1) 0px -7px 40px 0px inset, rgba(0, 0, 0, 0.06) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 1px 2px, rgba(0, 0, 0, 0.09) 0px 2px 4px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 10px 10px",
+              }}
             />
-            <p>{details.Plot}</p>
-            <p>
+            <p className="font-semibold mb-3 ">{details.Plot}</p>
+            <p className="font-semibold ">
               <strong>Director:</strong> {details.Director}
             </p>
-            <p>
+            <p className="font-semibold ">
               <strong>Cast:</strong> {details.Actors}
             </p>
-            <p>
+            <p className="font-semibold">
               <strong>Genre:</strong> {details.Genre}
             </p>
-            <p>
+            <p className="font-semibold">
               <strong>Release Date:</strong> {details.Released}
             </p>
           </>
         ) : (
-          <p>Loading...</p>
+          <p className="text-4xl font-bold ">Loading...</p>
         )}
       </div>
     </div>
