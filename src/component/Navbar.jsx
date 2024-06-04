45d73b7eb3311867adac6da4741514444f4c8cbd
juned1110 <junedking1110@gmail.com>
2024-06-04 14:35:06 +0530

    Navbar css updated

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { logout } from "../store/authSlice";
 import img from "../assets/img.png";
+import img1 from "../assets/search.png";
 
 const Navbar = ({ setSearchTerm }) => {
   const [term, setTerm] = React.useState("");
@@ -18,26 +19,31 @@ const Navbar = ({ setSearchTerm }) => {
   };
 
   return (
-    <nav className="p-4 bg-gray-800 text-white flex justify-between items-center">
+    <nav
+      className="p-2 bg-zinc-900 sticky top-0  text-white flex justify-between items-center rounded-full mx-3"
+      style={{
+        boxShadow: "rgb(38, 57, 77) 0px 20px 30px -10px",
+      }}
+    >
       <h1 className="text-2xl">
-        <img src={img} alt="Logo" className="w-20" />
+        <img src={img} alt="Logo" className="w-20 ml-5" />
       </h1>
       <form onSubmit={handleSearch} className="flex">
         <input
           type="text"
           value={term}
           onChange={(e) => setTerm(e.target.value)}
-          className="p-2 rounded-l-lg text-black w-[30vw]"
+          className="p-2 rounded-l-2xl rounded-r-2xl mr-3 text-black w-[30vw] "
         />
-        <button type="submit" className="p-2 bg-blue-500 rounded-r-lg w-[10vw]">
-          Search
+        <button type="submit" className="p-3 bg-white w-[3vw] rounded-full">
+          <img src={img1} alt="" />
         </button>
       </form>
       <div>
         {user ? (
           <button
             onClick={handleLogout}
-            className="p-2 bg-red-500 rounded-lg w-[10vw]"
+            className="p-[0.6rem]  rounded-lg w-[10vw] mr-5  bg-gradient-to-r bg-[#E90045] hover:bg-[#ff3a3a]"
           >
             Logout
           </button>
@@ -48,3 +54,4 @@ const Navbar = ({ setSearchTerm }) => {
 };
 
 export default Navbar;
+// bg-[#E90045]
